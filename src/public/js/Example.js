9d21382a8ac56df84e43a534d88def8588034edb
nakata-0523 <65937778+nakata-0523@users.noreply.github.com>
2022-11-15 09:28:00 +0900

    first commit

---
+++ src/public/js/Example.js
@@ -0,0 +1,169 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/components/Example.js":
+/*!********************************************!*\
+  !*** ./resources/js/components/Example.js ***!
+  \********************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /var/www/html/resources/js/components/Example.js: Support for the experimental syntax 'jsx' isn't currently enabled (26:9):\n\n\u001b[0m \u001b[90m 24 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 25 |\u001b[39m     \u001b[36mreturn\u001b[39m (\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 26 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33mFragment\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m         \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 27 |\u001b[39m             \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m className\u001b[33m=\u001b[39m\u001b[32m\"calender-header\"\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 28 |\u001b[39m                 \u001b[33m<\u001b[39m\u001b[33mh1\u001b[39m\u001b[33m>\u001b[39m{\u001b[32m`${year}年${month}月`\u001b[39m}\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mh1\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 29 |\u001b[39m                 \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m className\u001b[33m=\u001b[39m\u001b[32m\"calender-nav\"\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\nAdd @babel/preset-react (https://github.com/babel/babel/tree/main/packages/babel-preset-react) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-jsx) to the 'plugins' section to enable parsing.\n    at instantiate (/var/www/html/node_modules/@babel/parser/lib/index.js:67:32)\n    at constructor (/var/www/html/node_modules/@babel/parser/lib/index.js:364:12)\n    at Parser.raise (/var/www/html/node_modules/@babel/parser/lib/index.js:3364:19)\n    at Parser.expectOnePlugin (/var/www/html/node_modules/@babel/parser/lib/index.js:3413:18)\n    at Parser.parseExprAtom (/var/www/html/node_modules/@babel/parser/lib/index.js:11529:18)\n    at Parser.parseExprSubscripts (/var/www/html/node_modules/@babel/parser/lib/index.js:11171:23)\n    at Parser.parseUpdate (/var/www/html/node_modules/@babel/parser/lib/index.js:11153:21)\n    at Parser.parseMaybeUnary (/var/www/html/node_modules/@babel/parser/lib/index.js:11127:23)\n    at Parser.parseMaybeUnaryOrPrivate (/var/www/html/node_modules/@babel/parser/lib/index.js:10956:61)\n    at Parser.parseExprOps (/var/www/html/node_modules/@babel/parser/lib/index.js:10962:23)\n    at Parser.parseMaybeConditional (/var/www/html/node_modules/@babel/parser/lib/index.js:10937:23)\n    at Parser.parseMaybeAssign (/var/www/html/node_modules/@babel/parser/lib/index.js:10895:21)\n    at /var/www/html/node_modules/@babel/parser/lib/index.js:10863:39\n    at Parser.allowInAnd (/var/www/html/node_modules/@babel/parser/lib/index.js:12640:12)\n    at Parser.parseMaybeAssignAllowIn (/var/www/html/node_modules/@babel/parser/lib/index.js:10863:17)\n    at Parser.parseParenAndDistinguishExpression (/var/www/html/node_modules/@babel/parser/lib/index.js:11817:28)\n    at Parser.parseExprAtom (/var/www/html/node_modules/@babel/parser/lib/index.js:11446:23)\n    at Parser.parseExprSubscripts (/var/www/html/node_modules/@babel/parser/lib/index.js:11171:23)\n    at Parser.parseUpdate (/var/www/html/node_modules/@babel/parser/lib/index.js:11153:21)\n    at Parser.parseMaybeUnary (/var/www/html/node_modules/@babel/parser/lib/index.js:11127:23)\n    at Parser.parseMaybeUnaryOrPrivate (/var/www/html/node_modules/@babel/parser/lib/index.js:10956:61)\n    at Parser.parseExprOps (/var/www/html/node_modules/@babel/parser/lib/index.js:10962:23)\n    at Parser.parseMaybeConditional (/var/www/html/node_modules/@babel/parser/lib/index.js:10937:23)\n    at Parser.parseMaybeAssign (/var/www/html/node_modules/@babel/parser/lib/index.js:10895:21)\n    at Parser.parseExpressionBase (/var/www/html/node_modules/@babel/parser/lib/index.js:10845:23)\n    at /var/www/html/node_modules/@babel/parser/lib/index.js:10840:39\n    at Parser.allowInAnd (/var/www/html/node_modules/@babel/parser/lib/index.js:12635:16)\n    at Parser.parseExpression (/var/www/html/node_modules/@babel/parser/lib/index.js:10840:17)\n    at Parser.parseReturnStatement (/var/www/html/node_modules/@babel/parser/lib/index.js:13296:28)\n    at Parser.parseStatementContent (/var/www/html/node_modules/@babel/parser/lib/index.js:12954:21)\n    at Parser.parseStatement (/var/www/html/node_modules/@babel/parser/lib/index.js:12917:17)\n    at Parser.parseBlockOrModuleBlockBody (/var/www/html/node_modules/@babel/parser/lib/index.js:13497:25)\n    at Parser.parseBlockBody (/var/www/html/node_modules/@babel/parser/lib/index.js:13489:10)\n    at Parser.parseBlock (/var/www/html/node_modules/@babel/parser/lib/index.js:13477:10)\n    at Parser.parseFunctionBody (/var/www/html/node_modules/@babel/parser/lib/index.js:12287:24)\n    at Parser.parseFunctionBodyAndFinish (/var/www/html/node_modules/@babel/parser/lib/index.js:12271:10)\n    at /var/www/html/node_modules/@babel/parser/lib/index.js:13637:12\n    at Parser.withSmartMixTopicForbiddingContext (/var/www/html/node_modules/@babel/parser/lib/index.js:12617:14)\n    at Parser.parseFunction (/var/www/html/node_modules/@babel/parser/lib/index.js:13636:10)\n    at Parser.parseFunctionStatement (/var/www/html/node_modules/@babel/parser/lib/index.js:13276:17)\n    at Parser.parseStatementContent (/var/www/html/node_modules/@babel/parser/lib/index.js:12947:21)\n    at Parser.parseStatement (/var/www/html/node_modules/@babel/parser/lib/index.js:12917:17)\n    at Parser.parseBlockOrModuleBlockBody (/var/www/html/node_modules/@babel/parser/lib/index.js:13497:25)\n    at Parser.parseBlockBody (/var/www/html/node_modules/@babel/parser/lib/index.js:13489:10)\n    at Parser.parseProgram (/var/www/html/node_modules/@babel/parser/lib/index.js:12832:10)\n    at Parser.parseTopLevel (/var/www/html/node_modules/@babel/parser/lib/index.js:12822:25)\n    at Parser.parse (/var/www/html/node_modules/@babel/parser/lib/index.js:14674:10)\n    at parse (/var/www/html/node_modules/@babel/parser/lib/index.js:14716:38)\n    at parser (/var/www/html/node_modules/@babel/core/lib/parser/index.js:41:34)\n    at parser.next (<anonymous>)");
+
+/***/ }),
+
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/js/Example": 0,
+/******/ 			"css/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/components/Example.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
---
+++ src/resources/js/components/Example.js
@@ -0,0 +1,78 @@
+import React,{Fragment, useState} from 'react';
+import ReactDOM from 'react-dom';
+import { toUnicode } from 'punycode';
+
+function Example() {
+    const [year, setYear] = useState(new Date().getFullYear())
+    const [month, setMonth] = useState(new Date().getMonth() + 1)
+    const calendar = createCalendear(year,month)
+    const last = new Date(year, month, 0).getDate()
+    const prevlast = new Date(year, month-1, 0).getDate()
+
+    const onClick = n => () => {
+        const nextMonth = month + n
+
+        if (12 < nextMonth) {
+            setMonth(1)
+            setYear(year + 1)
+        } else if (nextMonth < 1) {
+            setMonth(12)
+            setYear(year - 1)
+        } else {
+            setMonth(nextMonth)
+        }
+    }
+
+    return (
+        <Fragment>
+            <div className="calender-header">
+                <h1>{`${year}年${month}月`}</h1>
+                <div className="calender-nav">
+                    <button onClick={onClick(-1)}>{'<先月'}</button>
+                    <button onClick={onClick(1)}>{'翌月>'}</button>
+                </div>
+            </div>
+            <table className="calender-table">
+                <thead>
+                    <tr>
+                        <th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {calendar.map((week,i) => (
+                        <tr key={week.json('')}>
+                            {week.map((day,j) => (
+                                <td key={`${i}${j}`} id={day} >
+                                    <div>
+                                        <div>
+                                            {day > last ? day - last : day <= 0 ? prevlast + day : day}
+                                        </div>
+                                        <div className="schedule-area">
+                                        </div>
+                                    </div>
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Fragment>
+    );
+}
+
+function createCalendear(year, month) {
+    const first = new Date(year, month - 1, 1).getDay()
+
+    return [0, 1, ,2, 3, 4, 5].map((weekIndex) => {
+        return [0, 1, 2, 3, 4, 5, 6].map((dayIndex) => {
+            const day = dayIndex + 1 + weekIndex * 7
+            return day - first
+        })
+    })
+}
+
+export default Example;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
+}
